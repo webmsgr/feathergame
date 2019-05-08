f98f5c8220f90e459c66f080a12c57190ef47302
webmsgr <zapcontact000@gmail.com>
2019-05-08 20:35:41 +0000

    Add stuff

--- game.py
+++ game.py
@@ -1,10 +1,24 @@
 """FeatherGame"""
 
 
+import pygame
+from pygame.locals import QUIT
+
 def main():
     """Main Game"""
-    print("Insert Game Code Here...")
-
+    background_colour = (255,255,255)
+    (width, height) = (300, 200)
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption('Window')
+    screen.fill(background_colour)
+    pygame.display.flip()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+    pygame.display.quit()
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
