2d510e9d7e11125fff471fa5f48b6a80c0ee6f4c
webmsgr <zapcontact000@gmail.com>
2019-05-10 18:15:29 +0000

    Game code

--- game.pyx
+++ game.pyx
@@ -31,30 +31,47 @@ cdef draw(map,tiles,int tilesize):
             drawx,drawy = tileloc(x,y,tilesize)
             newsurface.blit(tiles[map[y][x]],(drawx,drawy))
     return newsurface
-
+cdef blankmap(int size,tile=""):
+    cdef int i,j
+    out = []
+    for i in range(size):
+        blk = []
+        for j in range(size):
+            blk.append(tile)
+        out.append(blk)
+    return out
 cpdef main():
     """Main Game"""
+    DEF maxfps = 60
     DEF red = (255,0,0)
     DEF green = (0,255,0)
     DEF blue = (0,0,255)
     DEF white = (255,255,255)
     DEF black = (0,0,0)
+    DEF sctile = 8
+    DEF tilesize = 8
+    # screen is sctile/sctile tiles
     background_colour = (255,255,255)
-    (width, height) = (300, 200)
+    (width, height) = (sctile*tilesize, sctile*tilesize)
     screen = pygame.display.set_mode((width, height))
-    blanksurface = pygame.Surface((8,8))
+    blanksurface = pygame.Surface((tilesize,tilesize))
     blanksurface.fill(red)
+    blank = blankmap(sctile)
     pygame.display.set_caption('Window')
     screen.fill(background_colour)
     pygame.display.flip()
     running = True
+    clock = pygame.time.Clock()
     while running:
         screen.fill(background_colour)
-        screen.blit(draw([["",""],["",""]],{"":blanksurface},8),(0,0))#because i have no tiles prepared, this is blank
+        screen.blit(draw(blank,{"":blanksurface},tilesize),(0,0))
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == QUIT:
                 running = False
+                print(fps)
+        clock.tick(maxfps)
+        fps = clock.get_fps()
     pygame.display.quit()
     pygame.quit()
 
