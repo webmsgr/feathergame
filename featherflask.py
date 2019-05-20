f32803fe8a547c04edd1a5bc318bea5e1763d139
webmsgr <32279125+webmsgr@users.noreply.github.com>
2019-05-20 13:41:57 -0700

    Fix very bad thin

--- featherflask.py
+++ featherflask.py
@@ -6,7 +6,7 @@ import numpy
 def install(package):
     subprocess.call([sys.executable, "-m", "pip", "install", package])
 install("Flask")
-subprocess.call(["cython","game.pyx","--include_dir",numpy.get_include()])
+subprocess.call(["cython","game.pyx","-3","--include-dir",numpy.get_include()])
 from flask import Flask,redirect
 import os
 
