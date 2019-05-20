d55c536b98d224b8a81d84ad29f54c3758244989
webmsgr <32279125+webmsgr@users.noreply.github.com>
2019-05-20 11:36:25 -0700

    Update featherflask.py

--- featherflask.py
+++ featherflask.py
@@ -1,9 +1,16 @@
 # Used for heroku as a flask app
+import subprocess
+import sys
 
+def install(package):
+    subprocess.call([sys.executable, "-m", "pip", "install", package])
+install("Flask")
 from flask import Flask,redirect
 import os
+
 app = Flask(__name__, static_folder=".")
 @app.route("/")
 def home():
     return redirect("http://webmsgr.github.io/feathergame")
+
 app.run("0.0.0.0",os.environ["PORT"])
